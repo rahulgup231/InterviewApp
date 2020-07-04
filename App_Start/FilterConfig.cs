259b196379c5ddde47808329d1c44bc15b82b3ad
rahulkum123 <rahulgup231@gmail.com>
2020-07-04 14:22:27 +0530

    This is a new commit for what I originally planned to be amended

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MvcIdenity
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
